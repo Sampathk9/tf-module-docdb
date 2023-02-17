aafeaa1461ad618afd8e19b1872e0e977b215542
Raghu K <raghuk.vit@gmail.com>
2023-02-17 07:24:59 +0530

    Update tf-module-docdb/

--- main.tf
+++ main.tf
@@ -38,6 +38,7 @@ resource "aws_security_group" "docdb" {
 resource "aws_docdb_cluster" "docdb" {
   cluster_identifier     = "${var.env}-docdb-cluster"
   engine                 = "docdb"
+  engine_version         = var.engine_version
   master_username        = data.aws_ssm_parameter.DB_ADMIN_USER.value
   master_password        = data.aws_ssm_parameter.DB_ADMIN_PASS.value
   skip_final_snapshot    = true
